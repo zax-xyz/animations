c3e5124ee3e4313f0bca6e6bae95af8bb2d57fc4
Michael Vo <zax@zaxu.xyz>
2024-06-17 22:52:47 +1000

    kill

--- 2-more-transitions/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("toggle-btn");
-const content = document.getElementById("content");
-
-toggle.addEventListener("click", () => {
-  content.classList.toggle("active");
-});
